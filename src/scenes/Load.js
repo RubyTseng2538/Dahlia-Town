58abb9ac517dde847b59561e2a26b2790f45e2df
Ruby Tseng <ytseng7@ucsc.edu>
2022-05-27 16:51:15 -0700

    stuff

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -12,8 +12,6 @@ class Load extends Phaser.Scene{
         this.load.image('f', 'fkey.png')
         this.load.image('ground', 'ground.png');
         this.load.image('textbox', 'textbox.png');
-        this.load.image('p1', 'profile1.png');
-        this.load.image('p2', 'profile2.png');
         this.load.image('factoryentry', 'factoryentry.png');
         this.load.image('factory', 'factory.png');
         this.load.image('ground2', 'factoryfloor.png');
